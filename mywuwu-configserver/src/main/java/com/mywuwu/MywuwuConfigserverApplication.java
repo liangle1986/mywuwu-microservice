f9c6e99615508a67625d8b810a929fb80f655bd3
EricGan <1055078414@qq.com>
2018-09-12 11:29:57 +0800

    创建分布式配置文件服务

---
+++ mywuwu-configserver/src/main/java/com/mywuwu/MywuwuConfigserverApplication.java
@@ -0,0 +1,16 @@
+package com.mywuwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MywuwuConfigserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MywuwuConfigserverApplication.class, args);
+    }
+}
