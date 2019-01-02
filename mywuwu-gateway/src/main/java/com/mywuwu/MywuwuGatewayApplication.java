fead3149b2d57d74a35f425a1f4a29a0eddbe5c7
EricGan <1055078414@qq.com>
2019-01-02 12:42:10 +0800

    添加gateway

---
+++ mywuwu-gateway/src/main/java/com/mywuwu/MywuwuGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.mywuwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class MywuwuGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MywuwuGatewayApplication.class, args);
+    }
+
+}
+
